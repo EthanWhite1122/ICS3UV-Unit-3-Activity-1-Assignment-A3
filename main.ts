12355e8f1a5ff4e2c52f00d85ef0926805a5a304
Ethan White <ethan.white1@stu.ocsb.ca>
2025-11-18 13:40:44 +0000

    Finished assignment

---
+++ main.ts
@@ -0,0 +1,27 @@
+/**
+ * @author Ethan White
+ * @version 1.0.0
+ * @date 2025-11-14
+ * @fileoverview This program prints the multiplication table of 9.
+ */
+
+//Create variables
+const base: number = 9;
+const zero: number = 0;
+const one: number = 1;
+const two: number = 2;
+const three: number = 3;
+const four: number = 4;
+const five: number = 5;
+const six: number = 6;
+const seven: number = 7;
+const eight: number = 8;
+const nine: number = 9;
+const ten: number = 10;
+const eleven: number = 11;
+const twelve: number = 12;
+//OUTPUT
+
+console.log(zero + " X " + base + "=" + zero * base + "\n" + one + " X " + base + "=" + one * base + "\n" + two + " X " + base + "=" + two * base + "\n" + three + " X " + base + "=" + three * base + "\n" + four + " X " + base + "=" + four * base + "\n" + five + " X " + base + "=" + five * base + "\n" + six + " X " + base + "=" + six * base + "\n" + seven + " X " + base + "=" + seven * base + "\n" + eight + " X " + base + "=" + eight * base + "\n" + nine + " X " + base + "=" + nine * base + "\n" + ten + " X " + base + "=" + ten * base + "\n" + eleven + " X " + base + "=" + eleven * base + "\n" + twelve + " X " + base + "=" + twelve * base + "\n")
+
+console.log("\nDone")
